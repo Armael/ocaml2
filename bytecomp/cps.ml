3e5e73aefe60c869a37cd025a2692e4f7f3fc691
Armael <armael@isomorphis.me>
2015-10-22 16:08:41 +0100

    Fix order of evaluation

--- bytecomp/cps.ml
+++ bytecomp/cps.ml
@@ -98,16 +98,17 @@ open C
 let static_handlers = ref []
 
 let cps_eval_chain
+    ?(rev = false)
     (default_cont : cont_ident)
     (id_tms: (Ident.t * lambda_cps) list)
     (body: lambda):
   lambda
   =
-  List.fold_right (fun (id, tm) acc ->
+  List.fold_left (fun acc (id, tm) ->
     continue_with
       (mkcont ~std:(Clambda (id, acc)) default_cont)
       tm
-  ) id_tms body
+  ) body (if rev then List.rev id_tms else id_tms)
 
 let rec cps (already_cps: lambda -> bool) (tm: lambda): lambda_cps =
   let cps' = cps already_cps in
@@ -137,7 +138,7 @@ let rec cps (already_cps: lambda -> bool) (tm: lambda): lambda_cps =
                       info)))
       in
       abs_cont k
-        (cps_eval_chain k
+        (cps_eval_chain ~rev:true k
            (List.combine (fv :: args_idents) (cps' f :: args_cps))
            final_apply)
 
@@ -251,7 +252,7 @@ let rec cps (already_cps: lambda -> bool) (tm: lambda): lambda_cps =
           no_apply_info
         ) in
       abs_cont k
-        (cps_eval_chain k
+        (cps_eval_chain ~rev:true k
            (List.combine args_idents args_cps)
            final_apply)
 
@@ -448,7 +449,7 @@ let rec cps (already_cps: lambda -> bool) (tm: lambda): lambda_cps =
       )
     in
     abs_cont k
-      (cps_eval_chain k
+      (cps_eval_chain ~rev:true (* FIXME ? *) k
          (List.combine (objid :: methid :: args_id)
             ((cps' obj) :: (cps' meth) :: args_cps))
          final_apply)
